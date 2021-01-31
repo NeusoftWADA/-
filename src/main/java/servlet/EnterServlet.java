6f7eb8cec82a7bead0301184cedc7123c5af1864
qiuqianaw <qiuqianaw@163.com>
2021-02-01 02:23:28 +0800

    更新 EnterServlet逻辑

--- src/main/java/servlet/EnterServlet.java
+++ src/main/java/servlet/EnterServlet.java
@@ -2,12 +2,10 @@ package servlet;
 
 import entity.Knowledgedata;
 import entity.Set_categories;
-import entity.Sharedata;
 import entity.Userdata;
 import handler.CategoryHandler;
 import handler.EnterHandler;
 import handler.Set_categoriesHandler;
-import handler.ShareHandler;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,6 +15,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.Vector;
 
 @WebServlet("/EnterServlet")
 public class EnterServlet extends HttpServlet {
@@ -26,7 +25,7 @@ public class EnterServlet extends HttpServlet {
         response.setCharacterEncoding("utf-8");
         response.setHeader("content-type", "text/html;charset=utf-8");
 
-        //从前端获取username和password
+        //从前端获取文本内容
         String title = request.getParameter("title"); // 标题
         String Abstract = request.getParameter("Abstract"); // 摘要
         String content = request.getParameter("content"); // 内容
@@ -38,82 +37,52 @@ public class EnterServlet extends HttpServlet {
         knowledgedata.setAbstract(Abstract);
         knowledgedata.setContent(content);
 
-
-        int kid = 0;
-        int cid = 0;
-//        int uid = 0;
+        int kid;
+        int cid;
         /**
-         * 更新分类表
-         * TODO
-         * 更新后需返回一个当前分类的id
-         */
-        String cate[] = category.split(",");
-        try {
-            CategoryHandler categoryHandler = new CategoryHandler();
-
-            for ( String s : cate ) {
-                cid = categoryHandler.addCategory(s);
-            }
-
-            categoryHandler.closeConnection();
-            System.out.println("分类添加成功");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            System.out.println("分类添加失败");
-        }
-
-
-        /**
-         * 更新知识点表
-         * 更新以后需返回一个当前知识点的id
+         * 更新分类表 -> 更新知识点表 -> 更新设置分类表
+         *      更新分类表同时返回cid，储存在数组变量 Vector<Integer> set_cate 中
+         *      更新知识点表返回kid
+         *      遍历 Vector<Integer> set_cate，使用cid和kid更新设置分类表
          */
         PrintWriter printWriter = response.getWriter();
         HttpSession httpSession = request.getSession();
         //获取登录用户session
         Userdata user_session = (Userdata) httpSession.getAttribute("user_session");
-//        uid = user_session.getId();
-        try {
-            EnterHandler enterHandler = new EnterHandler();
-            kid = enterHandler.addKnowledge(user_session, knowledgedata);
-            printWriter.write("发布成功");
-            enterHandler.closeConnection();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-//        Sharedata sharedata = null;
-//        sharedata = new Sharedata(uid, kid);
-
-
+        //获取所有分类
+        String cate[] = category.split(",");
         //封装分类表
         Set_categories set_categories = null;
-        if ( kid == 0 || cid == 0 ) {
-            printWriter.println("----文件缺失！");
-        }
-        else {
-            set_categories = new Set_categories(kid, cid);
-        }
 
-        /**
-         * 更新设置分类表
-         */
         try {
-            Set_categoriesHandler set_categoriesHandler = new Set_categoriesHandler();
-            set_categoriesHandler.setCate(set_categories);
+            Vector<Integer> set_cate = new Vector<>();
+            CategoryHandler categoryHandler = new CategoryHandler();
+            for ( String s : cate ) {
+                cid = categoryHandler.addCategory(s);
+                set_cate.add(cid);
+            }
+
+            EnterHandler enterHandler = new EnterHandler();
+            kid = enterHandler.addKnowledge(user_session, knowledgedata);
+
+            for ( int n : set_cate) {
+                set_categories = new Set_categories(kid, n);
+                Set_categoriesHandler set_categoriesHandler = new Set_categoriesHandler();
+                set_categoriesHandler.setCate(set_categories);
+            }
+
+            enterHandler.closeConnection();
+            categoryHandler.closeConnection();
+
+            printWriter.write("发布成功<br>");
+            printWriter.write("分类添加成功<br>");
+            printWriter.write("设置分类成功<br>");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+            printWriter.write("ERROR<br>");
+            System.out.println("ERROR");
         }
 
-        /**
-         * 更新设置分享表
-         */
-//        try {
-//            ShareHandler shareHandler = new ShareHandler();
-//            shareHandler.setShare(sharedata);
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,7 @@
 import database.Database;
 
 import java.sql.SQLException;
+import java.util.Vector;
 
 public class Test {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -10,10 +11,15 @@ public class Test {
          */
 //        Database database = new Database();
 //        System.out.println(database.getConnection());
-        String str = "Java,C语言,Python";
-        String[] res = str.split(",");
-        for ( String s : res ) {
-            System.out.println(s);
-        }
+//        String str = "Java,C语言,Python";
+//        String[] res = str.split(",");
+//        for ( String s : res ) {
+//            System.out.println(s);
+//        }
+
+        Vector<Integer> set_cate = new Vector<>();
+        set_cate.add(1);
+        set_cate.add(2);
+        System.out.println(set_cate.get(0));
     }
 }
